8bfa814c2b376749146c9cbac4429fb1059c2c9a
katemhaven <luiscalzada123@gmail.com>
2016-11-01 17:38:49 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,36 @@
+var add = function(a, b) {
+  return a + b ;
+};
+
+var subtract= function(a, b) {
+  return a - b;
+};
+
+var multiply= function(a, b) {
+  return a * b;
+};
+
+var divide = function (a, b) {
+  return a/b ;
+};
+
+var inc = function(n) {
+  return n + 1;
+};
+
+var dec = function (n){
+  return n - 1;
+};
+
+var makeInt= 23;
+makeInt.toString();
+(23).toString();
+//(makeInt(a.toString())).toEqual(a)
+makeInt= ('0 * 2328')=0;
+
+
+
+
+
+
+
